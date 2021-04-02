66b308bba68cc9d227c672fa5f5d76efa8b5227b
John Pennycook <john.pennycook@intel.com>
2021-04-02 12:08:51 -0700

    [WIP] Working on PreprocessedSourcePrinter

--- codebasin/walkers/source_printer.py
+++ codebasin/walkers/source_printer.py
@@ -21,3 +21,24 @@ class SourcePrinter(TreeWalker):
 
         for child in node.children:
             self.__print_nodes(child)
+
+class PreprocessedSourcePrinter(TreeWalker):
+    """
+    TreeWalker that prints preprocessed source code.
+    """
+
+    def walk(self):
+        """
+        Walk the tree, printing the result of each node after preprocessing.
+        """
+        self.__print_nodes(self.tree.root)
+
+    def __print_nodes(self, node):
+        """
+        Print this specific node, then descend into its children nodes.
+        """
+        if type(node).__name__ != 'FileNode':
+            print(node.spelling())
+
+        for child in node.children:
+            self.__print_nodes(child)
